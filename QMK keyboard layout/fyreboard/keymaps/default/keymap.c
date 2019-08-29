445ad408be3956815cadf45cf26de815bc579007
Albert Luu <albert.luu.251@gmail.com>
2019-08-29 10:43:20 -0600

    Add updated QMK layout and 18mm knob model

--- QMK keyboard layout/fyreboard/keymaps/default/keymap.c
+++ QMK keyboard layout/fyreboard/keymaps/default/keymap.c
@@ -23,17 +23,41 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base, Osu mode */
-      KC_ESC, TO(1),  KC_ENTER, \
-      KC_S,  KC_D, KC_F, KC_J, KC_K, KC_L, \
+      KC_ESC, MO(6),  KC_ENTER, \
+      KC_S, KC_D, KC_F, KC_J, KC_K, KC_L, \
       KC_F2, KC_R, KC_SPACE, KC_SPACE, KC_DOT, KC_SLASH, \
       KC_LEFT, KC_RIGHT, KC_UP, KC_DOWN \
   ),
   [1] = LAYOUT( /* KSM/8k mode */
-      KC_ESC, TO(0), KC_ENTER, \
-      KC_S,  KC_D, KC_F, KC_J, KC_K, KC_L, \
-      KC_LEFT, KC_RIGHT, KC_C, KC_N, KC_UP, KC_DOWN, \
+      KC_ESC, KC_TRNS, KC_ENTER, \
+      KC_S, KC_D, KC_F, KC_J, KC_K, KC_L, \
+      KC_F2, KC_TAB, KC_C, KC_N, KC_PGUP, KC_PGDOWN, \
       KC_Q, KC_W, KC_O, KC_P \
-    )
+  ),
+  [2] = LAYOUT( /* Function Button */
+      KC_ESC, KC_TRNS, KC_ENTER, \
+      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, \
+      KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
+      KC_LEFT, KC_RIGHT, KC_UP, KC_DOWN \
+  ),
+  [3] = LAYOUT( /* Programmer mode */
+      KC_ESC, KC_TRNS, KC_ENTER, \
+      KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, \
+      KC_INS, KC_DEL, KC_HOME, KC_END, KC_PGUP, KC_PGDOWN, \
+      KC_LEFT, KC_RIGHT, KC_UP, KC_DOWN \
+  ),
+  [6] = LAYOUT( /* Layer Switching and audio control*/
+      KC_NO, KC_TRNS,  KC_NO, \
+      TO(0), TO(1), TO(2), TO(3), KC_NO, KC_NO, \
+      MO(7), KC_MUTE, KC_MPLY, KC_MSTP, KC_MPRV, KC_MNXT, \
+      KC_NO, KC_NO, KC_VOLD, KC_VOLU \
+  ),
+  [7] = LAYOUT( /* Reset Layer */
+      KC_NO, KC_TRNS,  KC_NO, \
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, RESET, \
+      KC_NO, KC_NO, KC_NO, KC_NO \
+  )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
